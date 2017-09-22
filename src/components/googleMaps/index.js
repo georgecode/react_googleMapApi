e9b7d2ca9d952539c3d4f2c43f7fcb0fc8281512
georgecode <georgeczurlanis@gmail.com>
2017-09-22 05:44:37 -0700

    Add map component

---
+++ src/components/googleMaps/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+class GoogleMap extends Component {
+	render(){
+		return(
+			<div>
+				<h1>Map will go here</h1>
+			</div>
+		)
+	}
+
+}
+
+
+
+
+export default GoogleMap;
